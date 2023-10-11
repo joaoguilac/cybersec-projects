65c28b59078b3ef9115e088c4e19b01e47d75490
joaoguilac <joguicosta@hotmail.com>
2023-10-11 01:15:53 -0300

    finish port scanner

--- network/port_scanner.py
+++ network/port_scanner.py
@@ -13,16 +13,18 @@ target = args.ip
 start_port = args.start
 end_port = args.end
 
+# Iterar por cada porta no intervalo fornecido
 for port in range(start_port, end_port+1):
+    # Criar um socket com protocolo IPV4 e TCP
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(1)
     try:
-        s.connect_ex((target, port))
+        s.connect((target, port))
         print(f"Port {port} is open")
         banner = s.recv(1024)
+        print("Entrou")
         if banner:
-            print(f"Port {banner.decode().strip()}")
+            print(f"Serviço {banner.decode().strip()}")
             s.close()
     except:
         pass
-        print(f"Port {port} is closed")
